dbe9dcd81936288366ef63e3fc6b78518c7a4cb0
Joshua Meadows <jmeadows4@mail.csuchico.edu>
2020-05-07 13:22:55 -0700

    add test for constructor

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -4,17 +4,31 @@
 
 #include <gtest/gtest.h>
 #include "Piezas.h"
- 
+
 class PiezasTest : public ::testing::Test
 {
 	protected:
 		PiezasTest(){} //constructor runs before each test
 		virtual ~PiezasTest(){} //destructor cleans up after tests
 		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
 TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+TEST(PiezasTest, checkConstructor)
+{
+  Piezas obj;
+  bool all_blanks = true;
+  for(int i = 0; i < BOARD_ROWS; i++){
+    for(int j = 0; j < BOARD_COLS; j++){
+      if(board[i][j] != Blank){
+        all_blanks = false;
+        break;
+      }
+    }
+  }
+  ASSERT_TRUE(all_blanks);
 }
